10189202293232a81c4fb3e23b033da898beabcb
AgriRouteco <AgriRoute.co@gmail.com>
2025-11-17 05:03:00 +0000

    Add files via upload

---
+++ pages/sell.js
@@ -0,0 +1,14 @@
+// pages/sell.js
+
+import React from 'react';
+
+const Sell = () => {
+  return (
+    <div>
+      <h1>Sell Page</h1>
+      <p>This page will use Supabase once the client and env variables are configured.</p>
+    </div>
+  );
+};
+
+export default Sell;
