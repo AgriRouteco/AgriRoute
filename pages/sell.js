6fc5f65a98234a1325422dce04e80581ee41b3bd
AgriRouteco <AgriRoute.co@gmail.com>
2025-11-17 04:23:02 +0000

    Update sell.js

--- pages/sell.js
+++ pages/sell.js
@@ -1,1 +1,1 @@
-# NOTE: This file expects supabase client in lib/supabaseClient.js and will work when env vars are configured
+// NOTE: This file expects supabase client in lib/supabaseClient.js and will work when env vars are configured
