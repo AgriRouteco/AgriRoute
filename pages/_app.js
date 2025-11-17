d620d033a06730b109f0141c2948a2d15411a8de
AgriRouteco <AgriRoute.co@gmail.com>
2025-11-17 07:14:12 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
-// pages/_app.js
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import { supabase } from '../lib/supabaseClient'
+import { useEffect, useState } from 'react'
 
 export default function MyApp({ Component, pageProps }) {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const session = supabase.auth.getUser().then(res => {
+      setUser(res.data.user)
+    })
+
+    supabase.auth.onAuthStateChange((event, session) => {
+      setUser(session?.user || null)
+    })
+  }, [])
+
   return (
-    <Layout>
-      <Component {...pageProps} />
+    <Layout user={user}>
+      <Component {...pageProps} user={user} />
     </Layout>
   )
 }
