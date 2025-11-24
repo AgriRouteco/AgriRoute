d31b8db6e0257860ef9d06db50588278487f70fa
AgriRouteco <AgriRoute.co@gmail.com>
2025-11-24 14:42:15 +0000

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -35,7 +35,7 @@ export default function Header({ user }) {
     <img 
       src="/logo.png"   // <-- update name if your file is different
       alt="AgriRoute Logo"
-      style={{ height: "100px", width: "auto" }}
+      style={{ height: "140px", width: "auto" }}
     />
   </Link>
 </div>
